3e4f77182b1416249c63eea1ce5868f39daf633c
z yf <1344195062@qq.com>
2026-05-16 17:39:03 +0800

    去掉调试信息

--- backend/web/views/create/character/get_list.py
+++ backend/web/views/create/character/get_list.py
@@ -16,7 +16,7 @@ class GetListCharacterView(APIView):
             user=User.objects.get(id=user_id)
             user_profile=UserProfile.objects.get(user=user)
             # -id表示倒序排 [items_count:items_count+20]左闭右开
-            characters_raw=Character.objects.filter(author=user_profile).order_by('-id')[items_count:items_count+1]
+            characters_raw=Character.objects.filter(author=user_profile).order_by('-id')[items_count:items_count+20]
             characters=[]
             for character in characters_raw:
                 author=character.author
